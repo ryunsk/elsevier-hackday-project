5e6d43f4b58996d56e312dc10fb06055fd4d0484
Ryun Shub Kim <ryun.shub.kim@gmail.com>
2021-10-29 10:09:18 +0100

    react router

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+<Router>
+  <Switch>
+    <Route path="/">
+      <App />
+    </Route>
+  </Switch>
+</Router>
 
 ReactDOM.render(
   <React.StrictMode>
