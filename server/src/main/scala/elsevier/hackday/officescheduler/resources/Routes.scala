36188e2a51a11a926ea5b8e1786e41148e170c36
Ryun Shub Kim <ryun.shub.kim@gmail.com>
2021-10-29 17:28:42 +0100

    Post method

--- server/src/main/scala/elsevier/hackday/officescheduler/resources/Routes.scala
+++ server/src/main/scala/elsevier/hackday/officescheduler/resources/Routes.scala
@@ -1,35 +1,42 @@
 package elsevier.hackday.officescheduler.resources
 
-import cats.effect.{Blocker, ContextShift, Sync}
-import elsevier.hackday.officescheduler.services.{SchedulerService, VersionService, UserData}
+import cats.effect.{Blocker, ContextShift, IO, Sync}
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion.User
+import elsevier.hackday.officescheduler.services.{SchedulerService, UserData, VersionService}
 import org.http4s._
 import org.http4s.dsl.Http4sDsl
-import org.http4s.implicits.http4sKleisliResponseSyntaxOptionT
+import org.http4s.implicits.{http4sKleisliResponseSyntaxOptionT, http4sLiteralsSyntax}
 import org.http4s.server.Router
 import org.http4s.server.staticcontent.resourceServiceBuilder
 import org.http4s.circe._
 import io.circe.generic.auto._
 import io.circe.syntax.EncoderOps
+import org.http4s.FormDataDecoder.formEntityDecoder
+import org.http4s.circe.CirceEntityCodec.circeEntityDecoder
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class Routes[F[_] : Sync : ContextShift](blocker: Blocker, versionService: VersionService, schedulerService: SchedulerService) {
 
+
+  //  val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
   private val apiRoutes: HttpRoutes[F] = {
+    implicit val decoder = jsonOf[IO, UserData]
     val dsl = new Http4sDsl[F] {}
     import dsl._
 
     HttpRoutes.of[F] {
       case GET -> Root / "version" =>
         Ok(versionService.version)
+
       case GET -> Root / "" =>
         Ok(schedulerService.userData.asJson)
-      case POST -> Root / "" =>
-        Ok()
-      //      case POST -> Root / "" =>
-      //        for{
-      //          userData <- req.as[UserData]
-      //          resp <- Ok(UserData(userData).asJson)
-      //        } yield (resp)
-      //    }.orNotFound
+
+      case req@POST -> Root / "" =>
+        req.decode[UserData] {
+          x => Ok(x.toString)
+        }
     }
   }
 
