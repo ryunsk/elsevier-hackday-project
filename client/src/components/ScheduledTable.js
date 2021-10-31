57548c9daa6a822d75b8f9548c184878b5948aeb
Ryun Shub Kim <ryun.shub.kim@gmail.com>
2021-10-31 23:47:23 +0000

    Clean up

--- client/src/components/ScheduledTable.js
+++ client/src/components/ScheduledTable.js
@@ -9,37 +9,27 @@ class ScheduledTable extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            tableData: [['2000-01-01', ['CCC', 'DDD']]]
+            tableData: []
         }
     }
 
     readDateAndName = async (term) => {
-        const arr = [['2000-01-01', ['AAA', 'BBB']]]
+        const arr = []
 
         scheduler.get('/users')
-            .then(console.log("Response data is: "))
             .then(response => {
                 Object.keys(response.data).map(key => arr.push([response.data[key].date, response.data[key].names]));
                 this.setState({ tableData: arr })
             })
-            // .then(this.setState({ tableData: arr }))
-            .then(console.log(arr))
-            .then(console.log(this.state.tableData))
-
-        // const response = await scheduler.get('/users')
-        // this.setState({
-        //     tableData: Object.keys(response.data).map(key => [response.data[key].date, response.data[key].names])
-        // })
     };
 
     componentDidMount() {
+        this.renderDataAsTable();
         this.readDateAndName();
     }
 
-    renderDataAsTable2() {
-        this.state.tableData.map((ourData) => {
-            console.log("TABLE DATA")
-            console.log(ourData)
+    renderDataAsTable() {
+        return this.state.tableData.map((ourData) => {
             return (
                 <Table.Row>
                     <Table.Cell>{ourData[0]}</Table.Cell>
@@ -49,16 +39,16 @@ class ScheduledTable extends React.Component {
         })
     }
 
-    renderDataAsTable() {
-        return this.props.resultDateAndName.map((ourData) => {
-            return (
-                <Table.Row>
-                    <Table.Cell>{ourData[0]}</Table.Cell>
-                    <Table.Cell>{ourData[1].join(", ")}</Table.Cell>
-                </Table.Row>
-            )
-        })
-    }
+    // renderDataAsTable2() {
+    //     return this.props.resultDateAndName.map((ourData) => {
+    //         return (
+    //             <Table.Row>
+    //                 <Table.Cell>{ourData[0]}</Table.Cell>
+    //                 <Table.Cell>{ourData[1].join(", ")}</Table.Cell>
+    //             </Table.Row>
+    //         )
+    //     })
+    // }
 
     render() {
         return (
@@ -76,17 +66,8 @@ class ScheduledTable extends React.Component {
                     </Table.Header>
                     <Table.Body>
                         {this.renderDataAsTable()}
-                        {this.renderDataAsTable2()}
-
-                        <Table.Row>
-                            <Table.Cell>Static test</Table.Cell>
-                            <Table.Cell>Static test</Table.Cell>
-                        </Table.Row>
-
                     </Table.Body>
                 </Table>
-                "Text data"
-                {this.state.tableData}
             </div>
         )
     }
