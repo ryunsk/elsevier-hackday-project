7a775eb2b5abcbbeee93c6320fcba1009d38b88b
Ryun Shub Kim <ryun.shub.kim@gmail.com>
2021-11-14 15:00:27 +0000

    Test to check return users

--- server/src/test/scala/elsevier/hackday/officescheduler/services/SchedulerServiceTest.scala
+++ server/src/test/scala/elsevier/hackday/officescheduler/services/SchedulerServiceTest.scala
@@ -1,33 +1,35 @@
 package elsevier.hackday.officescheduler.services
 
-import org.scalatest._
-import flatspec._
-import matchers._
-import org.scalatest.matchers.must.Matchers.be
-
+import org.scalatest.flatspec._
+import org.scalatest.matchers.should.Matchers
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 class SchedulerServiceTest extends AnyFlatSpec with Matchers {
 
   val schedulerService = new SchedulerService
-  ignore should "return all users" in {
-    // Override equals
+
+  behavior of "SchedulerService"
+
+  it should "return all users" in {
     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-    val defaultResult = Array(
+    val expectedResult = Array(
       UserData(LocalDate.parse("2021-10-28", formatter), Array("Steve", "Jon")),
       UserData(LocalDate.parse("2021-10-29", formatter), Array("Annie", "Shane"))
     )
-    schedulerService.getAllDatesAndUsers shouldBe defaultResult
+    schedulerService.getAllDatesAndUsers(0).names shouldBe expectedResult(0).names
+    schedulerService.getAllDatesAndUsers(1).names shouldBe expectedResult(1).names
   }
 
-  ignore should "return all users - negative" in {
+  it should "return all users - negative" in {
+    // Sanity check
     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-    val defaultResult = Array(
+    val expectedResult = Array(
       UserData(LocalDate.parse("2021-10-28", formatter), Array("aaa", "bbb")),
       UserData(LocalDate.parse("2021-10-29", formatter), Array("aaa", "bbb"))
     )
-    schedulerService.getAllDatesAndUsers should not be defaultResult
+    schedulerService.getAllDatesAndUsers(0).names should not be expectedResult(0).names
+    schedulerService.getAllDatesAndUsers(1).names should not be expectedResult(1).names
   }
 
   it should "add user" in {
