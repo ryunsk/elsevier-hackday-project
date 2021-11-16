c44186247e0a882a12ba148fee801e60f4eb581d
Ryun Shub Kim <ryun.shub.kim@gmail.com>
2021-11-16 13:25:30 +0000

    Small working example

--- server/src/main/scala/SimpleConnection.scala
+++ server/src/main/scala/SimpleConnection.scala
@@ -17,6 +17,17 @@ object SimpleConnection {
 
       // create the statement, and run the select query
       val statement = connection.createStatement()
+      val sqlSetup =
+        """
+          |DROP TABLE IF EXISTS USERS;
+          |
+          |CREATE TABLE USERS (date DATE, name VARCHAR);
+          |
+          |INSERT into USERS values ('2021-10-28', 'Steven');
+          |INSERT into USERS values ('2021-10-28', 'Jonathan');
+          |INSERT into USERS values ('2021-10-29', 'Violet');
+          |INSERT into USERS values ('2021-10-29', 'Powder');""".stripMargin
+      statement.execute(sqlSetup)
       val resultSet = statement.executeQuery("SELECT date, name FROM USERS")
       while (resultSet.next()) {
         val date = resultSet.getString("date")
